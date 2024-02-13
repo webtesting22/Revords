f49399fd2660ee26b04b4a0015d60e599f8e1b98
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-02-13 19:50:05 +0530

    integrated Home Page Modal with EmailJs

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ function SendMail() {
 
     // document.getElementById('contact-form').reset();
 }
+function homeModal() {
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        contact : document.getElementById("contact").value,
+        company : document.getElementById("company").value,
+        // title : document.getElementById("title").value,
+        zip : document.getElementById("zip").value
+    }
+    emailjs.send("service_0gba6iq", "template_bfi3xkp", params).then(function (res){
+        alert("Your form is submitted successfully!" )
+    })
+
+    // document.getElementById('contact-form').reset();
+}
