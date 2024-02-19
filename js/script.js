baffd2f37ab58e9fdc8078b057df9c925a1042f6
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-02-19 13:10:42 +0530

    index & contact Form validation error solved

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-function SendMail() {
-    var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        contact : document.getElementById("contact").value,
-        company : document.getElementById("company").value,
-        title : document.getElementById("title").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_0gba6iq", "template_00s3x1h", params).then(function (res){
-        alert("Your form is submitted successfully!" )
-    })
+// function SendMail() {
+//     var params = {
+//         name : document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//         contact : document.getElementById("contact").value,
+//         company : document.getElementById("company").value,
+//         title : document.getElementById("title").value,
+//         message : document.getElementById("message").value
+//     }
+//     emailjs.send("service_0gba6iq", "template_00s3x1h", params).then(function (res){
+//         alert("Your form is submitted successfully!" )
+//     })
 
-    // document.getElementById('contact-form').reset();
-}
-function homeModal() {
-    var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        contact : document.getElementById("contact").value,
-        company : document.getElementById("company").value,
-        // title : document.getElementById("title").value,
-        zip : document.getElementById("zip").value
-    }
-    emailjs.send("service_0gba6iq", "template_bfi3xkp", params).then(function (res){
-        alert("Your form is submitted successfully!" )
-    })
+//     // document.getElementById('contact-form').reset();
+// }
 
-    // document.getElementById('contact-form').reset();
-}
+
+// function homeModal() {
+//     var params = {
+//         name : document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//         contact : document.getElementById("contact").value,
+//         company : document.getElementById("company").value,
+//         // title : document.getElementById("title").value,
+//         zip : document.getElementById("zip").value
+//     }
+//     emailjs.send("service_0gba6iq", "template_bfi3xkp", params).then(function (res){
+//         alert("Your form is submitted successfully!" )
+//     })
+
+//     // document.getElementById('contact-form').reset();
+// }
